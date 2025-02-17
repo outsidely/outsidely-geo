3463ee9e822bfe9d918992283ca27e797e151822
Jesse <jesseamundsen@gmail.com>
2025-02-17 16:38:14 -0500

    fix typos

--- testGpx.sh
+++ testGpx.sh
@@ -6,4 +6,4 @@
 
 curl "https://outsidely-geo-app.azurewebsites.net/api/uploadActivityTest" -H "Content-Type: application/gpx+xml" -d "@$1" --output "$1.geojson"
 curl "https://outsidely-geo-app.azurewebsites.net/api/createStaticMap" -H "Content-Type: application/json" -d "@$1.geojson" --output "$1.png"
-curl "https://outsidely-geo-app.azurewebsites.net/api/createStatistics" -H "Content-Type: application/json" -d "@$1.geojson" --output "$1.json"
+curl "https://outsidely-geo-app.azurewebsites.net/api/createStatisticsData" -H "Content-Type: application/json" -d "@$1.geojson" --output "$1.json"
