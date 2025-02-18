12c4ea5557bfd4c285895bce00b7023792e0ab1e
Jesse <jesseamundsen@gmail.com>
2025-02-17 20:25:59 -0500

    completed new uploadActivity function to save blobs and create table entry

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@
 #https://stackoverflow.com/questions/47068504/where-to-find-python-implementation-of-chaikins-corner-cutting-algorithm
 #  useful for smoothing distance
 
-#https://en.wikipedia.org/wiki/Spline_interpolation
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.CubicSpline.html#scipy.interpolate.CubicSpline
 #  useful for smoothing elevation
 
 #https://fitdecode.readthedocs.io/en/latest/
@@ -24,6 +24,8 @@ import math
 import uuid
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import os
+from azure.data.tables import TableServiceClient, TableClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -32,7 +34,7 @@ def createJsonHttpResponse(statusCode, message, properties = {}):
     response["statusCode"] = statusCode
     response["message"] = message
     for k,v in properties.items():
-        if (k not in ["statusCode", "message"]):
+        if (k in ["statusCode", "message"]):
             raise ValueError("Properties cannot be named statusCode or message")
         else:
             response[k] = v
@@ -92,6 +94,21 @@ def parseStatisticsData(activityData):
     statisticsData["descent"] = descent
 
     return statisticsData
+
+def saveBlob(data, name):
+    blob_service_client = BlobServiceClient.from_connection_string(os.environ["storageaccount_connectionstring"])
+    blob_client = blob_service_client.get_blob_client("outsidelycontainer",name)
+    blob_client.upload_blob(data)
+
+def upsertEntity(table, entity):
+    try:
+        partitionKey = entity["PartitionKey"]
+        rowkey = entity["RowKey"]
+    except:
+        raise ValueError("PartitionKey and RowKey are required for entities")
+    table_service_client = TableServiceClient.from_connection_string(os.environ["storageaccount_connectionstring"])
+    table_client = table_service_client.get_table_client(table)
+    table_client.upsert_entity(entity)
 
 #curl "http://localhost:7071/api/uploadActivityTest" -H "Content-Type: application/gpx+xml" -d "@/home/jesse/Desktop/test.gpx" --output "/home/jesse/Desktop/test.geojson"
 @app.route(route="uploadActivityTest")
@@ -154,9 +171,14 @@ def uploadActivity(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('called uploadActivity')
 
-    activityId = str(uuid.uuid4())
-
     try:
+
+        activityId = str(uuid.uuid4())
+        upsertEntity("activities", {
+            "PartitionKey": "userId",
+            "RowKey": activityId,
+        })
+
         if (req.headers["Content-Type"] == "application/gpx+xml"):
 
             # convert to geojson
@@ -164,7 +186,6 @@ def uploadActivity(req: func.HttpRequest) -> func.HttpResponse:
             data_frame = pyogrio.read_dataframe(request_body, layer="track_points")
             geojson_out = BytesIO()
             pyogrio.write_dataframe(data_frame, geojson_out, driver="GeoJSON", layer="track_points")
-            #return func.HttpResponse(geojson_out.getvalue(), status_code=200, mimetype="application/geo+json")
 
             # convert to activityModel
             activityData = parseActivityData(json.loads(geojson_out.getvalue().decode()))
@@ -183,14 +204,27 @@ def uploadActivity(req: func.HttpRequest) -> func.HttpResponse:
             image = m.render()
             image.save(response_data, format="png")
 
-            # save file, geojson, activitymodel to storage container
+            # save file, geojson, activityData, preview to storage container
+            saveBlob(request_body, activityId + "/source.gpx")
+            saveBlob(geojson_out, activityId + "/geojson.json")
+            saveBlob(json.dumps(activityData).encode(), activityId + "/activityData.json")
+            saveBlob(response_data.getvalue(), activityId + "/preview.png")
 
             # save statistics + staticmap to tblsvc
-            
+            upsertEntity("activities", {
+                "PartitionKey": "userId",
+                "RowKey": activityId,
+                "time": statisticsData["time"],
+                "distance": statisticsData["distance"],
+                "ascent": statisticsData["ascent"],
+                "descent": statisticsData["descent"]
+            })
+
             # return 
-            return createJsonHttpResponse(200, "Successfully processed activity", {"activityId": activityId})
+            return createJsonHttpResponse(201, "Successfully created activity", {"activityId": activityId})
         
         else:
             return createJsonHttpResponse(415, "Unsupported Content-Type")
-    except:
-        return createJsonHttpResponse(500, "Backend Error")
+    except Exception as ex:
+        return createJsonHttpResponse(500, str(ex))
+    
--- test.py
+++ test.py
@@ -1,13 +1,27 @@
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import json
+import os
+from azure.data.tables import TableServiceClient, TableClient
+import datetime
 
-def createJsonHttpResponse(statusCode, message, properties = {}):
-    
-    response = {}
-    response["statusCode"] = statusCode
-    response["message"] = message
-    for k,v in properties.items():
-        if (k not in ["statusCode", "message"]):
-            raise ValueError("Properties cannot be named statusCode or message")
-        else:
-            response[k] = v
-    return json.dumps(response)
+os.environ["storageaccount_connectionstring"] = "DefaultEndpointsProtocol=https;AccountName=outsidelystorage;AccountKey=41GZ5nit1vilCAa+nBxwGy9hvRz5WMsGugWGI2ICv3+XcFpxy/5z03i/AbKr7yvFl5l6mYgLJxJa+AStTw8fOw==;EndpointSuffix=core.windows.net"
+
+PRODUCT_ID = u'001234'
+PRODUCT_NAME = u'RedMarker'
+
+my_entity = {
+    u'PartitionKey': PRODUCT_NAME,
+    u'RowKey': PRODUCT_ID,
+    u'Stock': 15,
+    u'Price': 9.99,
+    u'Comments': u"great product",
+    u'OnSale': True,
+    u'ReducedPrice': 7.99,
+    u'PurchaseDate': datetime.datetime(1973, 10, 4),
+    u'BinaryRepresentation': b'product_name'
+}
+
+table_service_client = TableServiceClient.from_connection_string(os.environ["storageaccount_connectionstring"])
+table_client = table_service_client.get_table_client("activities")
+table_client.upsert_entity(my_entity)
