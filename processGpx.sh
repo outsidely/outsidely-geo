f19c1b49c970fd4c5817073a3ce770ddfac62386
Jesse Amundsen <jesseamundsen@costquest.com>
2025-02-23 21:12:44 -0500

    update old script var names to be correct

--- processGpx.sh
+++ processGpx.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
 if [ $# -eq 0 ]; then
-  echo "Usage: ./processGpx.sh <gpxfile> <userid> <secret> <activitytype=run,ride,other>"
+  echo "Usage: ./processGpx.sh <gpxfile> <userid> <password> <activitytype=run,ride,other>"
   exit 1
 fi
 
-curl "https://outsidely-geo-app.azurewebsites.net/api/upload" -F upload="@$1" -F userid="$2" -F secret="$3" -F activitytype="$4" --output -
+curl "https://outsidely-geo-app.azurewebsites.net/api/upload" -F upload="@$1" -F userid="$2" -F password="$3" -F activitytype="$4" --output -
